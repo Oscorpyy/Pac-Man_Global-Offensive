22c777ede3334debd9655441700071a02c933824
Leo Goderneaux <lgoderne@z3r7p5.42lyon.fr>
2026-07-30 11:27:27 +0200

    [FEAT] can render text, and with multiline possibility

--- src/drawing_methods.py
+++ src/drawing_methods.py
@@ -1,6 +1,7 @@
 from numpy import ndarray
 import sdl2
 import ctypes
+import sdl2.sdlttf as sttf
 
 
 def put_pixels(pixels_array, x, y, width, height, color) -> None:
@@ -30,3 +31,15 @@ def draw_sprite_sheet(renderer, img_texture, x: int, y: int, frame: int, scale: 
     dest_rect = sdl2.SDL_Rect(x, y, 32 * scale, 32 * scale)
     src_rect = sdl2.SDL_Rect(frame_to_draw, 0, 32, 32)
     sdl2.SDL_RenderCopy(renderer, img_texture, ctypes.byref(src_rect), ctypes.byref(dest_rect))
+
+def draw_text(renderer, font, text: str, scale: int) -> None:
+    len_text: int = len(text)
+    text_split: list = text.split(b"\n")
+    for i in range(len(text_split)):
+        text_surface = sttf.TTF_RenderText_Solid(font, text_split[i], sdl2.SDL_Color(255, 255, 255, 255))
+        line_w = text_surface.contents.w
+        line_h = text_surface.contents.h
+        text_texture = sdl2.SDL_CreateTextureFromSurface(renderer, text_surface)
+        sdl2.SDL_FreeSurface(text_surface)
+        dest_rect = sdl2.SDL_Rect(0,line_h * i * scale, line_w * scale, line_h * scale)
+        sdl2.SDL_RenderCopy(renderer, text_texture, None, ctypes.byref(dest_rect))
--- src/scene/main_menu/main_menu.py
+++ src/scene/main_menu/main_menu.py
@@ -1,13 +1,16 @@
 import sdl2
 import sdl2.sdlimage as sdim
-from src.drawing_methods import draw_rect_full, clear_background, draw_sprites, draw_sprite_sheet
+import sdl2.sdlttf as sttf
+import numpy as np
+import ctypes
+from src.drawing_methods import draw_rect_full, clear_background, draw_text, draw_sprite_sheet
 from src.scene.helper import get_ptr
 from src.print_logs import print_error
-import numpy as np
 
 class MainMenu:
     def __init__(self, renderer, width: int, height: int) -> None:
         self.renderer = renderer
+        # img loading
         sdim.IMG_Init(sdim.IMG_INIT_PNG)
         self.img_path = b"assets/test3.png"
         self.img_surface = sdim.IMG_Load(self.img_path)
@@ -15,6 +18,13 @@ class MainMenu:
             print_error(f"can't charge image {sdim.IMG_GetError()}")
         self.img_texture = sdl2.SDL_CreateTextureFromSurface(self.renderer, self.img_surface)
         sdl2.SDL_FreeSurface(self.img_surface)
+        # Font loading
+        sttf.TTF_Init()
+        self.font_size = 16
+        self.font = sttf.TTF_OpenFont(b"assets/Press_Start_2P/PressStart2P-Regular.ttf", self.font_size)
+        if not self.font:
+            print_error(f"can't charge font {sttf.TTF_GetError()}")
+        self.text = b"aaaaaaaaaa\nadsfasdfsadf"
         self.width = width
         self.background = sdl2.SDL_CreateTexture(
             renderer,
@@ -29,8 +39,11 @@ class MainMenu:
         self.blue = 0xFF0000FF
         self.red = 0xFFFF0000
 
+
     def clean_up(self) -> None:
         sdim.IMG_Quit()
+        sttf.TTF_CloseFont(self.font)
+        sttf.TTF_Quit()
 
 
     def draw_background(self) -> None:
@@ -41,6 +54,7 @@ class MainMenu:
         sdl2.SDL_RenderCopy(self.renderer, self.background, None, None)
         # draw_sprites(self.renderer, self.img_texture, 50, 45, 10)
         draw_sprite_sheet(self.renderer, self.img_texture, 50, 45, 0, 4)
+        draw_text(self.renderer, self.font, self.text, 2)
         sdl2.SDL_RenderPresent(self.renderer)
         self.test_x += 10
         if self.test_x >= self.width:
